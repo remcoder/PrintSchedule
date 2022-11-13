e5d6f38cab728e6e58040f0aef13cf488884d31c
remcoder <remcoder@users.noreply.github.com>
2022-11-13 21:34:59 +0100

    Add firebase adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
-
+import firebase from 'svelte-adapter-firebase';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: firebase()
 	}
 };
 
